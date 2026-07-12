7b3ae811658e8211588d7019509a1dabfea0d79f
Jonatas Jose da Silva <jonatasolosdeguitarra@gmail.com>
2026-07-12 10:28:17 -0300

    API com categorias de noticias

--- servidor/bot.py
+++ servidor/bot.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import requests
@@ -15,8 +16,10 @@ CORS(app)
 API_KEY = os.getenv("NEWS_API_KEY")
 
 
+
 @app.route("/")
 def home():
+
     return """
     <h1>🚀 NEWS BRASIL NOTÍCIAS ONLINE</h1>
     <p>Servidor funcionando!</p>
@@ -24,9 +27,12 @@ def home():
 
 
 
-def buscar_noticias():
+
+def buscar_noticias(categoria="brasil"):
+
 
     if not API_KEY:
+
         return [{
             "titulo": "API não configurada",
             "resumo": "Arquivo .env não encontrado",
@@ -35,50 +41,116 @@ def buscar_noticias():
         }]
 
 
-    url = (
-        "https://newsapi.org/v2/everything"
-        "?q=Brasil"
-        "&language=pt"
-        "&sortBy=publishedAt"
-        "&pageSize=10"
-        f"&apiKey={API_KEY}"
+
+    categorias = {
+
+
+        "brasil": "Brasil",
+
+        "mundo": "mundo",
+
+        "politica": "política",
+
+        "economia": "economia",
+
+        "tecnologia": "tecnologia",
+
+        "esportes": "esportes",
+
+        "entretenimento": "entretenimento"
+
+
+    }
+
+
+
+    termo = categorias.get(
+        categoria,
+        "Brasil"
     )
 
 
-    resposta = requests.get(url, timeout=15)
+
+    url = (
+
+        "https://newsapi.org/v2/everything"
+
+        f"?q={termo}"
+
+        "&language=pt"
+
+        "&sortBy=publishedAt"
+
+        "&pageSize=10"
+
+        f"&apiKey={API_KEY}"
+
+    )
+
+
+
+    resposta = requests.get(
+        url,
+        timeout=15
+    )
+
+
 
     dados = resposta.json()
 
 
+
     print("====================")
+    print("CATEGORIA:", categoria)
     print("STATUS NEWS API:", resposta.status_code)
-    print(dados)
     print("====================")
+
 
 
     noticias = []
 
 
+
     for item in dados.get("articles", []):
+
 
         noticias.append({
 
+
             "titulo": item.get("title"),
+
             "resumo": item.get("description"),
+
             "imagem": item.get("urlToImage"),
+
             "link": item.get("url")
 
+
         })
+
 
 
     return noticias
 
 
 
+
+
 @app.route("/noticias")
 def noticias():
 
-    return jsonify(buscar_noticias())
+
+    categoria = request.args.get(
+        "categoria",
+        "brasil"
+    )
+
+
+    return jsonify(
+        buscar_noticias(categoria)
+    )
+
+
 
 
 
@@ -86,17 +158,28 @@ def noticias():
 def api():
 
     return jsonify({
+
         "status":"online"
+
     })
+
+
 
 
 
 if __name__ == "__main__":
 
+
     print("🚀 NEWS BRASIL NOTÍCIAS ONLINE")
 
+
     app.run(
+
         host="0.0.0.0",
+
         port=5000,
+
         debug=True
+
     )
+```
